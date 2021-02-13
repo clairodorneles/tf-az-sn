6378d5c21f069738c90dcb15ce1b547b43b37447
Clairo Dorneles <clairo@Clairos-MacBook-Pro.local>
2021-02-13 11:59:30 -0300

    bug03: corrected outputs

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@
 
 output "subnet" {
     value = {
-      for subnet, address_prefix in var.listSubnets :
+      for subnet, address_prefix in var.subnets :
         subnet => {
           name            = subnet
           id              = azurerm_subnet.subnet[subnet].id
